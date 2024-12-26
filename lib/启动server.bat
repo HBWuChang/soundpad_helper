79dcf23c78a6e289c46494a48c697ad6b398f18a
HBWuChang <2243162077@qq.com>
2024-12-26 23:25:04 +0800

    readme

--- lib/启动server.bat
+++ lib/启动server.bat
@@ -1,1 +1,2 @@
-D:/Python312/python.exe e:/HB_WuChang/code/flutter/soundpad_helper/lib/server.py
+python ./server.py
+@REM D:/Python312/python.exe e:/HB_WuChang/code/flutter/soundpad_helper/lib/server.py
